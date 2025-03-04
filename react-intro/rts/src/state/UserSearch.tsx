f735ab2dc76326278c632b9fb1f57df9c8b01961
Bryce Monaco <brycedmonaco@gmail.com>
2025-03-03 22:11:53 -0800

    Implemented a simple interactive user search from a hard coded user list

---
+++ react-intro/rts/src/state/UserSearch.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+const users = [
+  { name: 'Samus', armor: 'Varia'},
+  { name: 'Master Chief', armor: 'Mk VI'},
+  { name: 'Isaac', armor: 'Engineer Rig'}
+]
+
+const UserSearch: React.FC = () => {
+  const [name, setName] = useState('');
+  const [user, setUser] = useState<{ name: string, armor: string } | undefined>();
+
+  const onClick = () => {
+    const foundUser = users.find((user) => {
+      return user.name === name;
+    });
+
+    setUser(foundUser);
+  };
+
+  return <div>
+    User Search
+    <input value={name} onChange={(e) => setName(e.target.value)} />
+    <button onClick={onClick}>Find User</button>
+    <div>
+      {user && user.name} wears {user && user.armor}
+    </div>
+  </div>
+};
+
+export default UserSearch;
